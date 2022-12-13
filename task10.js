b9dc4306bf42106cdcd11c14bf5e296d153d6240
davidiglesiasgomez <37771705+davidiglesiasgomez@users.noreply.github.com>
2022-12-13 13:00:33 +0100

    Task10

--- task10.js
+++ task10.js
@@ -1,5 +1,43 @@
 function checkJump(heights) {
-    return false
+    if (heights.length === 0) {
+        return false
+    }
+
+    let estado = ''
+    return heights.every((v, i, a) => {
+        if (estado === '' && i === a.length-1) {
+            return false
+        }
+        if (estado === 'subiendo' && i === a.length-1) {
+            return false
+        }
+        if (estado === 'bajando' && i === a.length-1) {
+            return true
+        }
+        if (estado === '' && v > a[i+1]) {
+            return false
+        }
+        if (estado === '' && v < a[i+1]) {
+            estado = 'subiendo'
+            return true
+        }
+        if (estado === 'subiendo' && v <= a[i+1]) {
+            estado = 'subiendo'
+            return true
+        }
+        if (estado === 'subiendo' && v > a[i+1]) {
+            estado = 'bajando'
+            return true
+        }
+        if (estado === 'bajando' && v < a[i+1]) {
+            return false
+        }
+        if (estado === 'bajando' && v > a[i+1]) {
+            estado = 'bajando'
+            return true
+        }
+        return true
+    })
 }
 
 module.exports = checkJump
--- task10.test.js
+++ task10.test.js
@@ -1,9 +1,85 @@
 const checkJump = require('./task10')
 
+test('[1, 2, 1]', () => {
+    expect(checkJump([1, 2, 1])).toBe(true)
+})
+
 test('[1, 3, 8, 5, 2]', () => {
     expect(checkJump([1, 3, 8, 5, 2])).toBe(true)
 })
 
 test('[1, 7, 3, 5]', () => {
     expect(checkJump([1, 7, 3, 5])).toBe(false)
+})
+
+test('[1, 2, 3, 2, 1]', () => {
+    expect(checkJump([1, 2, 3, 2, 1])).toBe(true)
+})
+
+test('[1, 2, 2, 2, 1]', () => {
+    expect(checkJump([1, 2, 2, 2, 1])).toBe(true)
+})
+
+test('[0, 1, 0]', () => {
+    expect(checkJump([0, 1, 0])).toBe(true)
+})
+
+test('[2, 2, 2, 2]', () => {
+    expect(checkJump([2, 2, 2, 2])).toBe(false)
+})
+
+test('[1, 2, 3]', () => {
+    expect(checkJump([1, 2, 3])).toBe(false)
+})
+
+test('[1, 2, 3, 2, 1, 2, 3]', () => {
+    expect(checkJump([1, 2, 3, 2, 1, 2, 3])).toBe(false)
+})
+
+test('[1, 1000, 900, 800]', () => {
+    expect(checkJump([1, 1000, 900, 800])).toBe(true)
+})
+
+test('[1, 1, 1, 1, 1, 1, 1, 1, 2, 1]', () => {
+    expect(checkJump([1, 1, 1, 1, 1, 1, 1, 1, 2, 1])).toBe(true)
+})
+
+test('[1, 2, 3, 1, 3, 1]', () => {
+    expect(checkJump([1, 2, 3, 1, 3, 1])).toBe(false)
+})
+
+test('[1, 3, 2, 5, 4, 3, 2, 1]', () => {
+    expect(checkJump([1, 2, 3, 1, 3, 1])).toBe(false)
+})
+
+test('[1]', () => {
+    expect(checkJump([1])).toBe(false)
+})
+
+test('[3, 2, 1]', () => {
+    expect(checkJump([3, 2, 1])).toBe(false)
+})
+
+test('[2, 2, 1]', () => {
+    expect(checkJump([2, 2, 1])).toBe(false)
+})
+
+test('[3, 2, 1, 2, 3]', () => {
+    expect(checkJump([3, 2, 1, 2, 3])).toBe(false)
+})
+
+test('[1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1]', () => {
+    expect(checkJump([1, 2, 3, 2, 1, 0, 1, 2, 3, 2, 1])).toBe(false)
+})
+
+test('[1, 2, 1, 2]', () => {
+    expect(checkJump([1, 2, 1, 2])).toBe(false)
+})
+
+test('[4, 4, 3, 2, 1]', () => {
+    expect(checkJump([4, 4, 3, 2, 1])).toBe(false)
+})
+
+test('[]', () => {
+    expect(checkJump([])).toBe(false)
 })
