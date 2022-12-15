6a19a68bb235c7591c502ce04043597147be9434
davidiglesiasgomez <37771705+davidiglesiasgomez@users.noreply.github.com>
2022-12-15 14:38:31 +0100

    Task14

--- task14.js
+++ task14.js
@@ -1,5 +1,35 @@
 function getOptimalPath(path) {
-    return 0
+    let paths = []
+    let segmentos = []
+    let temporal_paths = []
+    let valores = []
+    for (let i=0; i<path.length; i++) {
+        segmentos = []
+        temporal_paths = []
+        for (let j=0; j<path[i].length; j++) {
+
+            if (paths.length == 0) {
+                paths.push([[j, i]])
+                temporal_paths = paths
+                continue
+            }
+
+            paths.forEach(element => {
+                if (j != element.at(-1)[0] && j != 1+element.at(-1)[0]) {
+                    return
+                }
+                segmentos = element.concat([[j, i]])
+                temporal_paths = temporal_paths.concat([segmentos])
+            })
+        }
+        paths = [...temporal_paths]
+    }
+
+    valores = paths.map(e1 => {
+        return e1.reduce((acc, e2) => acc + path[e2[1]][e2[0]], 0)
+    })
+
+    return Math.min(...valores)
 }
 
 module.exports = getOptimalPath
