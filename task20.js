d2cce0c9e0dec9bfe41a719aa8699bb6d58ed237
davidiglesiasgomez <david.iglesias@gmail.com>
2022-12-25 12:39:37 +0100

    Task20

--- task20.js
+++ task20.js
@@ -1,89 +1,36 @@
 function howManyReindeers(reindeerTypes, gifts) {
-    const comprobarCoincidenciaValor = (valor, pesos, iteracion) => {
-
-        // console.log('valor', valor, 'iteracion', iteracion, 'reduce', iteracion.reduce((acc, e, i) => {
-        //     return acc + e * pesos[i]
-        // }, 0))
-
-        return valor == iteracion.reduce((acc, e, i) => {
-            return acc + e * pesos[i]
-        }, 0)
-    }
-
-    const comprobarOrdenCorrecto = (iteracion) => {
-        return !iteracion.some((e, i, a) => {
-            if (a[i+1] === undefined) return false
-            return e > a[i+1]
-        })
-    }
-
-    const restarUnidad = (maximos, iteracion) => {
-        let carry=1
-        iteracion = iteracion.reverse().map((e, i) => {
-            if (carry == 1 && e-1 < 0) {
-                carry = 1
-                return maximos[i]
-            } else if (carry == 1) {
-                carry = 0
-                return e-1
-            }
-            carry = 0
-            return e
-        }).reverse()
-        return iteracion
-    }
-
     const calcularRenos = (weight) => {
-        // console.log('reindeerTypes', reindeerTypes)
-        // console.log('weight', weight)
+        reindeerTypes.sort((a, b) => b.weightCapacity - a.weightCapacity)
 
-        let pesos = reindeerTypes.map(reindeer => reindeer.weightCapacity)
+        reindeerTypes.forEach((_, i) => {
+            reindeerTypes[i].num = 0
+        })
 
-        let maximos = reindeerTypes.map(reindeer => ~~(weight/reindeer.weightCapacity))
-        // let maximos = reindeerTypes.map(reindeer => reindeer.weightCapacity)
-        // console.log('maximos', maximos)
-
-        let iteracion = maximos
-        while (iteracion.some(e => e != 0)) {
-            iteracion = restarUnidad(maximos, iteracion)
-            if (!comprobarOrdenCorrecto(iteracion)) {
-                continue
+        let carry = weight
+        reindeerTypes.forEach((e, i, a) => {
+            let sum = a.filter((_, j) => j>=i).reduce((acc, e2) => acc + e2.weightCapacity, 0)
+            if (e.weightCapacity > carry) return
+            let factor = ~~(carry / sum)
+            carry = carry % sum
+            for (let j=i; j<reindeerTypes.length; j++) {
+                reindeerTypes[j].num = reindeerTypes[j].num + factor
             }
-            if (!comprobarCoincidenciaValor(weight, pesos, iteracion)) {
-                continue
-            }
-            // console.log('iteracion', iteracion)
-            break;
-        }
+        });
 
-        return reindeerTypes.map((reindeer, i) => {
+        return reindeerTypes.map((reindeer) => {
             return {
                 type: reindeer.type,
-                num: iteracion[i]
+                num: reindeer.num || 0
             }
         }).filter(e => e.num != 0)
     }
 
     return gifts.map(gift => {
-        // console.log(gift)
         return {
             country: gift.country,
             reindeers: calcularRenos(gift.weight)
         }
     })
 }
-
-// const reindeerTypes = [
-//     { type: 'Nuclear', weightCapacity: 50 },
-//     { type: 'Electric', weightCapacity: 10 },
-//     { type: 'Gasoline', weightCapacity: 5 },
-//     { type: 'Diesel', weightCapacity: 1 }
-// ]
-// const gifts = [
-//     { country: 'Spain', weight: 30 },
-//     // { country: 'France', weight: 17 },
-//     // { country: 'Italy', weight: 50 }
-// ]
-// console.log('howManyReindeers', howManyReindeers(reindeerTypes, gifts)[0])
 
 module.exports = howManyReindeers
